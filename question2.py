0f1e3937a193ef2ca2c9fbf135b978a108e9dc0c
kao910019 <kao910019@gmail.com>
2019-11-02 14:48:24 +0800

    Update question2.py

--- question2.py
+++ question2.py
@@ -46,4 +46,22 @@ matrix2 = tf.constant(
          [1,2,3,4,5,6,7,0,0,0],
          [1,2,3,4,5,6,7,8,9,0]])
 
-# --- Write your code below --- #
+# Rebuild input matrix.
+matrix = tf.concat([matrix1,matrix2],axis=1)
+shape = tf.shape(matrix)
+# Indices use to divide vector.
+num_partial = 10
+odd_indices = tf.matmul(tf.reshape(tf.range(1, shape[0]*2, 2), [-1, 1]), tf.fill([1, shape[1]], 1))
+even_indices = tf.matmul(tf.reshape(tf.range(0, shape[0]*2, 2), [-1, 1]), tf.fill([1, shape[1]], 1))
+# Partial 0 and other number, then recombine it.
+target = tf.where(tf.not_equal(matrix, 0), even_indices, odd_indices)
+sequence_parts = tf.dynamic_partition(matrix, target, num_partial)
+matrix3 = tf.reshape(tf.concat(sequence_parts, axis=0), shape)
+
+with tf.Session() as sess:
+    matrix1, matrix2, matrix3 = sess.run([matrix1, matrix2, matrix3])
+    print("matrix1 =", matrix1)
+    print("matrix2 =", matrix2)
+    print("matrix3 =", matrix3)
+    
+    
