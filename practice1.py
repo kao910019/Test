0483c489db89c4e9ba219325c8ec15566d50a4f5
kao910019 <kao910019@gmail.com>
2019-11-02 15:43:12 +0800

    Update and rename question1.py to practice1.py

--- practice1.py
+++ practice1.py
@@ -4,26 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #source data
-t = np.arange(0, 720)
-y = np.sin(t*(np.pi/180))
+x = np.arange(0, 720)
+y = np.sin(x*(np.pi/180))
 
 #placeholder
-time_placeholder = tf.placeholder(tf.float32, [None])
-value_placeholder = tf.placeholder(tf.float32, [None])
+x_placeholder = tf.placeholder(tf.float32, [None])
+y_placeholder = tf.placeholder(tf.float32, [None])
 
 # change shape to [batch_size, 1]
-time = tf.expand_dims(time_placeholder,axis=1)
-value = tf.expand_dims(value_placeholder,axis=1)
+bx = tf.expand_dims(x_placeholder,axis=1)
+by = tf.expand_dims(y_placeholder,axis=1)
 
 #Full connect layer
-full_connect1 = tf.layers.dense(time, 50, activation=tf.nn.tanh)
+full_connect1 = tf.layers.dense(bx, 50, activation=tf.nn.tanh)
 full_connect2 = tf.layers.dense(full_connect1, 25, activation=tf.nn.tanh)
 full_connect3 = tf.layers.dense(full_connect2, 50, activation=tf.nn.tanh)
 full_connect4 = tf.layers.dense(full_connect3, 25, activation=tf.nn.tanh)
-predict_value = tf.layers.dense(full_connect4, 1, activation=tf.nn.tanh)
+predict_y = tf.layers.dense(full_connect4, 1, activation=tf.nn.tanh)
 
 #loss function MAE
-loss = tf.losses.absolute_difference(value, predict_value)
+loss = tf.losses.absolute_difference(by, predict_y)
 
 #minimize
 optimizer = tf.train.AdamOptimizer(8e-4)
@@ -35,13 +35,13 @@ with tf.Session() as sess:
     
     #training 10000 times
     for epoch in range(20000):
-        output_time, output_value, output_predict, _ = sess.run(
-                [time, value, predict_value, train_op],
-                 feed_dict={time_placeholder: t, value_placeholder: y})
+        output_x, output_y, output_predict, _ = sess.run(
+                [bx, by, predict_y, train_op],
+                 feed_dict={x_placeholder: x, y_placeholder: y})
         
         #print it
         if epoch % 1000 == 0:
             plt.figure()
-            plt.plot(output_time, output_value)
-            plt.plot(output_time, output_predict)
+            plt.plot(output_x, output_y)
+            plt.plot(output_x, output_predict)
             plt.show()
